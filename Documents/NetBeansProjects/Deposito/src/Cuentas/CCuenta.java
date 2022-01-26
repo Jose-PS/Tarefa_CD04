63af731b3ad4b2ffbff7ffab0b0acd28a499ef27
droeco <josemps@pm.me>
2022-01-26 21:33:20 +0100

    Actualizar tarefa

---
+++ Documents/NetBeansProjects/Deposito/src/Cuentas/CCuenta.java
@@ -0,0 +1,130 @@
+package Cuentas;
+
+public class CCuenta {
+
+    /**Obtemos o nome
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**Modificamos o nome
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**Obtemos a conta
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**Modificamos a conta
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**Obtemos o saldo
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**Modificamos o saldo
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**Obtemos o tipo de interes
+     * @return the tipoInterés
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**Modificamos o tipo de interes
+     * @param tipoInterés the tipoInterés to set
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+    /**
+     * Definimos as variables da conta
+     */
+    
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+    
+    /**
+     * Constructor da conta
+     */
+
+    public CCuenta()
+    {
+    }
+    
+    /**
+     * Constructor da conta con parametros
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
+    
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+    
+    /**
+     * Obtemos o estado da conta
+     * @return Devolve o saldo da conta
+     */
+    
+    public double estado()
+    {
+        return getSaldo();
+    }
+    
+    /**
+     * Ingresa unha cantidade na conta
+     * @param cantidad
+     * @throws Exception 
+     */
+    
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+    
+    /**
+     * Retira unha cantidade da conta
+     * @param cantidad
+     * @throws Exception 
+     */
+    
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+}
---
+++ Documents/NetBeansProjects/Deposito/src/Cuentas/Main.java
@@ -0,0 +1,29 @@
+package Cuentas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta(0);
+    }
+
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
