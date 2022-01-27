698566161dec4b92fde1f6acfe90a189061a56c3
droeco <josemps@pm.me>
2022-01-27 13:01:03 +0100

    Actualizar tarefa

--- Documents/NetBeansProjects/Deposito/src/Cuentas/CCuenta.java
+++ Documents/NetBeansProjects/Deposito/src/Cuentas/CCuenta.java
@@ -2,56 +2,72 @@ package Cuentas;
 
 public class CCuenta {
 
-    /**Obtemos o nome
+    /**
+     * Obtemos o nome
+     *
      * @return devolve o nome
      */
     public String getNombre() {
         return nombre;
     }
 
-    /**Modificamos o nome
+    /**
+     * Modificamos o nome
+     *
      * @param nombre the nombre to set
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**Obtemos a conta
+    /**
+     * Obtemos a conta
+     *
      * @return the cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
-    /**Modificamos a conta
+    /**
+     * Modificamos a conta
+     *
      * @param cuenta the cuenta to set
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
-    /**Obtemos o saldo
+    /**
+     * Obtemos o saldo
+     *
      * @return the saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
-    /**Modificamos o saldo
+    /**
+     * Modificamos o saldo
+     *
      * @param saldo the saldo to set
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-    /**Obtemos o tipo de interes
+    /**
+     * Obtemos o tipo de interes
+     *
      * @return the tipoInterés
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-    /**Modificamos o tipo de interes
+    /**
+     * Modificamos o tipo de interes
+     *
      * @param tipoInterés the tipoInterés to set
      */
     public void setTipoInterés(double tipoInterés) {
@@ -61,70 +77,66 @@ public class CCuenta {
     /**
      * Definimos as variables da conta
      */
-    
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-    
+
     /**
      * Constructor da conta
      */
-
-    public CCuenta()
-    {
+    public CCuenta() {
     }
-    
+
     /**
      * Constructor da conta con parametros
+     *
      * @param nom
      * @param cue
      * @param sal
-     * @param tipo 
+     * @param tipo
      */
-    
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
-    
+
     /**
      * Obtemos o estado da conta
+     *
      * @return Devolve o saldo da conta
      */
-    
-    public double estado()
-    {
+    public double estado() {
         return getSaldo();
     }
-    
+
     /**
      * Ingresa unha cantidade na conta
+     *
      * @param cantidad
-     * @throws Exception 
+     * @throws Exception
      */
-    
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
-    
+
     /**
      * Retira unha cantidade da conta
+     *
      * @param cantidad
-     * @throws Exception 
+     * @throws Exception
      */
-    
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 }
